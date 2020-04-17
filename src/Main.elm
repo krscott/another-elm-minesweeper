20368120ddb0f0f935ea191371f98f3e5a91e036
Kris Scott <kscott91@gmail.com>
2020-04-17 07:04:11 -0500

    Add mine randomization

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (Model, Msg, init, main, update, view)
+module Main exposing (main)
 
 import Array
 import Array2D exposing (Array2D)
@@ -6,25 +6,38 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
+import List.Extra exposing (zip)
+import Random exposing (generate)
+import Random.List exposing (shuffle)
+
+
+
+-- MAIN
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
+    Browser.element
         { init = init
         , view = view
         , update = update
+        , subscriptions = subscriptions
         }
+
+
+
+-- MODEL
 
 
 type CellRevealedState
     = Default
     | Revealed Int
+    | RevealedMine
 
 
 type alias CellState =
     { revealed : CellRevealedState
-    , isBomb : Bool
+    , isMine : Bool
     }
 
 
@@ -32,31 +45,89 @@ type alias Model =
     Array2D CellState
 
 
-init : Model
-init =
-    Array2D.initialize 10
-        10
+initModel : Int -> Int -> Model
+initModel rows cols =
+    Array2D.initialize
+        rows
+        cols
         (\_ _ ->
             { revealed = Default
-            , isBomb = False
+            , isMine = False
             }
         )
 
 
-type Msg
-    = ClickRC Int Int
-    | Reset
-    | DebugRevealAll
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initModel 10 10, Cmd.none )
+
+
+
+-- UPDATE
+
+
+allCoords : Int -> Int -> List ( Int, Int )
+allCoords rows cols =
+    List.range 0 (rows - 1)
+        |> List.map
+            (\r ->
+                List.range 0 (cols - 1)
+                    |> List.map (\c -> ( r, c ))
+            )
+        |> List.concat
+
+
+setMine : Int -> Int -> Array2D CellState -> Array2D CellState
+setMine r c grid =
+    let
+        newCell =
+            case Array2D.get r c grid of
+                Just cell ->
+                    { cell | isMine = True }
+
+                Nothing ->
+                    { revealed = Default
+                    , isMine = True
+                    }
+    in
+    Array2D.set r c newCell grid
+
+
+setMines : Int -> List ( Int, Int ) -> Array2D CellState -> Array2D CellState
+setMines n coords grid =
+    List.take n coords
+        |> List.foldl (\( r, c ) acc -> setMine r c acc) grid
 
 
 revealCell : Array2D CellState -> Int -> Int -> CellState -> CellState
 revealCell _ r c cell =
-    { cell | revealed = Revealed (modBy 9 (r + c)) }
+    { cell
+        | revealed =
+            if cell.isMine then
+                RevealedMine
+
+            else
+                Revealed (modBy 9 (r + c))
+    }
 
 
-update : Msg -> Model -> Model
+type Msg
+    = RandomizeBombs
+    | SetBombs (List ( Int, Int ))
+    | ClickRC Int Int
+    | Reset
+    | DebugRevealAll
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        RandomizeBombs ->
+            ( model, generate SetBombs (shuffle (allCoords 10 10)) )
+
+        SetBombs coords ->
+            ( setMines 10 coords model, Cmd.none )
+
         ClickRC r c ->
             let
                 maybeCell =
@@ -64,25 +135,31 @@ update msg model =
             in
             case maybeCell of
                 Nothing ->
-                    model
+                    ( model, Cmd.none )
 
                 Just cell ->
                     case cell.revealed of
                         Default ->
-                            Array2D.set
+                            ( Array2D.set
                                 r
                                 c
                                 (revealCell model r c cell)
                                 model
+                            , Cmd.none
+                            )
 
                         _ ->
-                            model
+                            ( model, Cmd.none )
 
         Reset ->
-            init
+            ( initModel 10 10, Cmd.none )
 
         DebugRevealAll ->
-            Array2D.indexedMap (\r c cell -> revealCell model r c cell) model
+            ( Array2D.indexedMap
+                (\r c cell -> revealCell model r c cell)
+                model
+            , Cmd.none
+            )
 
 
 cellButtonView : List (Attribute msg) -> String -> Html msg
@@ -102,11 +179,28 @@ cellView row col state =
         Revealed n ->
             cellButtonView [ class "revealed" ] (String.fromInt n)
 
+        RevealedMine ->
+            cellButtonView [ class "revealed" ] "💣"
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
 
 view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Reset ] [ text "Reset" ]
+        , button [ onClick RandomizeBombs ] [ text "Randomize" ]
         , button [ onClick DebugRevealAll ] [ text "DEBUG: Reveal All" ]
         , div [ class "gameboard" ]
             (List.indexedMap
