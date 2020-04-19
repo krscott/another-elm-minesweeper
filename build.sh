3dfb5030cbf0639f771ee9e1e337e323c303384f
Kris Scott <kscott91@gmail.com>
2020-04-19 18:06:56 -0500

    Add docs directory for GitHub Page

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!bash
 
-mkdir -p dist/dist
-mkdir -p dist/src
-elm make src/Main.elm --output dist/dist/main.js
-cp src/index.html dist
-cp src/style.css dist/src
+mkdir -p docs/dist
+mkdir -p docs/src
+elm make src/Main.elm --output docs/dist/main.js
+cp src/index.html docs/
+cp src/style.css docs/src
